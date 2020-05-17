4b8219e3d9f3aca1fe20f533d78d2b6f701bb5b4
Nathan Duff <me@nateduff.com>
2020-05-17 10:45:32 -0400

    Remove Admin mistake

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = class extends React.Component {
 
     const openAdmin = event => {
       event.preventDefault();
-      
+
       window.location.href = "https://duffsitestore.z14.web.core.windows.net/admin/#"
     }
 
@@ -109,7 +109,7 @@ const Navbar = class extends React.Component {
               </Link>
               <BrowserView>
               {this.props.isAuthenticated ? 
-              <Link className="navbar-item" onClick={openAdmin}>
+              <Link className="navbar-item" id="adminButton">
                 Admin
               </Link> : null}</BrowserView>
               {this.props.isAuthenticated ? 

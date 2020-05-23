2abfe91856866425ec8fedd5303e34ae8cbddacd
NateDuff <nate.duff@outlook.com>
2020-05-23 09:23:43 -0400

    Colors and favicon

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -37,17 +37,19 @@ export const IndexPageTemplate = ({
           justifyContent: 'space-around',
           alignItems: 'left',
           flexDirection: 'column',
+          textAlign: 'center',
         }}
       >
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              'rgb(24, 69, 59, 0.54) 0.5rem 0px 0px, rgb(24, 69, 59, 0.54) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(24, 69, 59, 0.54)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
+            textAlign: 'center',
           }}
         >
           {title}
@@ -56,8 +58,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              'rgb(24, 69, 59, 0.54) 0.5rem 0px 0px, rgb(24, 69, 59, 0.54) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(24, 69, 59, 0.54)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',

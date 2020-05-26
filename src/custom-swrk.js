e38dbb8718b134bd547773332a8f661a5c314b2e
NateDuff <nate.duff@outlook.com>
2020-05-26 15:42:52 -0400

    SW update to remove CDN calls

--- src/custom-swrk.js
+++ src/custom-swrk.js
@@ -1,8 +1,8 @@
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
+importScripts('firebase-app.js');
+importScripts('firebase-messaging.js');
 
 firebase.initializeApp({
-  'messagingSenderId': '275924356890'
+  'messagingSenderId': `${process.env.firebase_messagingSenderId}`
 });
 
 const messaging = firebase.messaging();

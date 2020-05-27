1889597f994207c6673208793bc1ea814f42840d
NateDuff <nate.duff@outlook.com>
2020-05-27 00:14:34 -0400

    Realign firebase versions

--- src/custom-swrk.js
+++ src/custom-swrk.js
@@ -1,8 +1,15 @@
-importScripts('firebase-app-sw.js');
-importScripts('firebase-messaging-sw.js');
+importScripts('firebase-app.js');
+importScripts('firebase-messaging.js');
 
 firebase.initializeApp({
-  'messagingSenderId': `${process.env.firebase_messagingSenderId}`
+  'messagingSenderId': `${process.env.firebase_messagingSenderId}`,
+  'apiKey': `${process.env.firebase_apiKey}`,
+  'authDomain': `${process.env.firebase_authDomain}`,
+  'databaseURL': `${process.env.firebase_databaseURL}`,
+  'projectId': `${process.env.firebase_projectId}`,
+  'storageBucket': `${process.env.firebase_storageBucket}`,
+  'messagingSenderId': `${process.env.firebase_messagingSenderId}`,
+  'appId': `${process.env.firebase_appId}`
 });
 
 const messaging = firebase.messaging();

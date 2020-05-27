862e8ac4e683921f81590b22d566a8709d304a4f
NateDuff <nate.duff@outlook.com>
2020-05-27 19:41:45 -0400

    Auth fix

--- src/components/Authorization.js
+++ src/components/Authorization.js
@@ -1,7 +1,8 @@
 const isBrowser = typeof window !== `undefined`
 const isReadyForAuth = typeof _adalInstance !== `undefined`
 
-const adal = _adalInstance
+const adal = typeof _adalInstance !== `undefined` ?
+    _adalInstance : null;
 
 export const isLoggedIn = () => {
     if (!isBrowser) return false
@@ -15,7 +16,7 @@ export const isAdmin = () => {
   if (!isBrowser) return false
   if (!isReadyForAuth) return false
 
-  return adal?._user?.profile.upn.endsWith("@nateduff.com")
+  return adal?._user?.profile.upn.endsWith("@nateduff.com") || process.env.NODE_ENV === 'development'
     ? true : false
 }
 

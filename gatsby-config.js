27b5c70554b42a71e05c11e3a90b652bea683f03
NateDuff <nate.duff@outlook.com>
2020-06-08 22:14:31 -0400

    Color favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         background_color: "#18453b",
         theme_color: "#18453b",
         display: "standalone",
-        icon: "src/img/logo.png",
+        icon: "src/img/logo-color.png",
         crossOrigin: `use-credentials`,
         gcm_sender_id: "275924356890"
       },

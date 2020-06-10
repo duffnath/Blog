72c3c8972485580b9520bdb296848d0499587029
NateDuff <nate.duff@outlook.com>
2020-06-09 21:26:01 -0400

    Loggedin

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -122,7 +122,7 @@ export const BlogPostTemplate = ({
               <div className="column social">
                 {
                   <a title="Send Push Notification" onClick={() => sendPushNotification()} hidden={
-                    isAdmin() ? false : true
+                    isLoggedIn() && isAdmin() ? false : true
                   }>
                   <img
                     src={bell}

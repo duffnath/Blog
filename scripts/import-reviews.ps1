e2367c2aa079a45eae9a3e7d9384580a2a257664
NateDuff <nate.duff@outlook.com>
2020-06-11 21:01:28 -0400

    Importing google reviews during build

---
+++ scripts/import-reviews.ps1
@@ -0,0 +1,34 @@
+## Script to update Procuts page with reviews from Google
+param (
+    $projRoot = "$HOME/Sources/repos/gatsby-starter-netlify-cms"
+)
+
+$pageLocation = "$projRoot/src/pages/products/index.md"
+
+$productsContent = Get-Content $pageLocation -Raw
+
+$testimonialsIndex = $productsContent.IndexOf("testimonials:")
+
+## Get reviews
+$placeID = $ENV:google_placeID
+$apiKey = $ENV:google_apiKey
+$queryString = "place_id=$placeID&fields=name,rating,formatted_phone_number,reviews&key=$apiKey"
+
+$placeUri = "https://maps.googleapis.com/maps/api/place/details/json?$queryString"
+
+$reviews = ((IWR $placeUri).Content | Convertfrom-Json).result.reviews
+
+## Append reviews to site page
+$content = ""
+
+$content += "testimonials:`n"
+
+foreach ($review in $reviews) {
+    $content += "  - author: $($review.author_name)`n"
+    $content += "    quote: >-`n"
+    $content += "      $($review.text)`n"
+}
+
+$content += "---"
+
+Set-Content $pageLocation -Value ($productsContent.substring(0,$testimonialsIndex) + $content)

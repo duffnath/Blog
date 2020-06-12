e1327d09d26c8377b33dd9924ba34fbfcadaf5af
NateDuff <nate.duff@outlook.com>
2020-06-11 22:53:27 -0400

    Google api fix

--- scripts/import-reviews.ps1
+++ scripts/import-reviews.ps1
@@ -1,4 +1,9 @@
 ## Script to update Procuts page with reviews from Google
+param (
+    $placeID = $ENV:google_placeID,
+    $apiKey = $ENV:google_apiKey
+)
+
 $pageLocation = "./src/pages/products/index.md"
 
 $productsContent = Get-Content $pageLocation -Raw
@@ -6,8 +11,6 @@ $productsContent = Get-Content $pageLocation -Raw
 $testimonialsIndex = $productsContent.IndexOf("testimonials:")
 
 ## Get reviews
-$placeID = "$(google_placeID)"
-$apiKey = "$(google_apiKey)"
 $queryString = "place_id=$placeID&fields=name,rating,formatted_phone_number,reviews&key=$apiKey"
 
 $placeUri = "https://maps.googleapis.com/maps/api/place/details/json?$queryString"
